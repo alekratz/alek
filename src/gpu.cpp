7fe1f0d87018ec1ceb8e9354608eee05cbbc92a9
Alek Ratzloff <alekratz@gmail.com>
2015-05-04 18:29:18 -0400

    Add some GPU code. things will likely change here, like the offsets

--- src/gpu.cpp
+++ src/gpu.cpp
@@ -1,1 +1,21 @@
 #include "gpu.h"
+#include "types.h"
+#include "mailbox.h"
+
+Graphics::Graphics()
+{
+
+}
+
+Graphics::~Graphics()
+{
+
+}
+
+FramebufferInfo *fb_info = nullptr;
+
+void init_framebuffer()
+{
+  //u32 fb_address = 
+  //write_mailbox()
+}
--- src/gpu.h
+++ src/gpu.h
@@ -1,4 +1,33 @@
 #ifndef AAA_GPU_H
 #define AAA_GPU_H
 
+#include "types.h"
+
+#define GPU_FLUSH_CACHE 0x40000000
+#define FB_BASE_ADDR 0x2000
+
+struct FramebufferInfo
+{
+  u32 p_width = 1024;   // physical width
+  u32 p_height = 768;   // physical height
+  u32 v_width = 1024;   // virtual width
+  u32 v_height = 786;   // virtual height
+  u32 gpu_pitch = 0;    // gpu pitch
+  u32 bit_depth = 16;   // bit depth
+  u32 x = 0;            // x offset
+  u32 y = 0;            // y offset
+  void* gpu_ptr = 0;    // frame pointer
+  u32 gpu_size = 0;     // buffer size
+};
+
+class Graphics
+{
+public:
+  Graphics();
+  ~Graphics();
+};
+
+extern FramebufferInfo *fb_info;
+void init_framebuffer();
+
 #endif
