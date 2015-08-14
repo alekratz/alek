28e1b11d3a4745dcc49bafde8936d077eb9e0d4d
Alek Ratzloff <alekratz@gmail.com>
2015-08-14 11:13:44 -0400

    Fixed the comments to be the correct format (i.e. using /* */ rather than #) and also add some experimental kernel startup arguments for memory information

--- arch/i686-elf/boot.s
+++ arch/i686-elf/boot.s
@@ -1,56 +1,72 @@
-#
-# Copyright (C) 2015 Alek Ratzloff
-# 
-# This file is part of Alek's Little Endian Kernel.
-# 
-# Alek's Little Endian Kernel is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# 
-# Alek's Little Endian Kernel is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with Alek's Little Endian Kernel.  If not, see <http://www.gnu.org/licenses/>.
-# 
+/* 
+* Copyright (C) 2015 Alek Ratzloff
+* 
+* This file is part of Alek's Little Endian Kernel.
+* 
+* Alek's Little Endian Kernel is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* Alek's Little Endian Kernel is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with Alek's Little Endian Kernel.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
-# Constants used for creating a multiboot header
-.set ALIGN,   1 << 0              # align loaded modules on page boundaries
-.set MEMINFO, 1 << 1              # provide memory map
-.set FLAGS,   ALIGN | MEMINFO     # multiboot 'flag' field
-.set MAGIC,   0x1BADB002          # 'magic number' lets bootloader find the header
-.set CHECKSUM, -(MAGIC + FLAGS)   # checksum of above
+#define ASM_FILE 1
+/* Constants used for creating a multiboot header */
+.set ALIGN,   1 << 0              /* align loaded modules on page boundaries */
+.set MEMINFO, 1 << 1              /* provide memory map */
+.set FLAGS,   ALIGN | MEMINFO     /* multiboot 'flag' field */
+.set MAGIC,   0x1BADB002          /* 'magic number' lets bootloader find the header */
+.set CHECKSUM, -(MAGIC + FLAGS)   /* checksum of above */
 
-# Multiboot header
+/* Multiboot header */
 .section .multiboot
 .align 4
 .long MAGIC
 .long FLAGS
 .long CHECKSUM
+header_addr:
+.long 0
+load_addr:
+.long 0
+load_end_addr:
+.long 0
+bss_end_addr:
+.long 0
+entry_addr:
+.long 0
 
-# Set up the stack
+/* Set up the stack */
 .section bootstrap_stack, "aw", @nobits
 stack_bottom:
-.skip 16384 # 16 KB
+.skip 16384 /* 16 KB */
 stack_top:
 
-# text section
+/* text section */
 .section .text
 .global start
 .type start, @function
 start:
-  # Set %esp
+  /* Set %esp */
   movl $stack_top, %esp
-  # Jump into the kernel
+  push entry_addr
+  push bss_end_addr
+  push load_end_addr
+  push load_addr
+  push header_addr
+  /* Jump into the kernel */
   call kmain
-  # After the kernel exits, be sure to call __cxa_finalize
+  /* After the kernel exits, be sure to call __cxa_finalize */
   push 0
   call __cxa_finalize
 
-  # Hang
+  /* Hang */
   cli
   hlt
 .Lhang:
