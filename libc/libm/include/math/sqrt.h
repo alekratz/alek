2cc4280cda753e7fdbac24d256c3e1d899c78900
Alek Ratzloff <alekratz@gmail.com>
2015-08-16 23:20:21 -0400

    Update sqrt method name to the __sqrt64() convention

--- libc/libm/include/math/sqrt.h
+++ libc/libm/include/math/sqrt.h
@@ -6,13 +6,13 @@
 /**
  * Architecture implemented square root
  */
-extern "C" f64 __arch_sqrt64(f64 num);
+extern "C" f64 __sqrt64(f64 num);
 
 /**
  * "Lazy" version of square root
  */
 template<typename num_t>
 num_t sqrt(num_t num)
-{ return __arch_sqrt64(static_cast<f64>(num)); }
+{ return __sqrt64(static_cast<f64>(num)); }
 
 #endif /* ALEK_LIBM_SQRT */
