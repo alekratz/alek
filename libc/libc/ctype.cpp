21c9e14b22c9503f589c0d51c6c5301dccb009ea
Alek Ratzloff <alekratz@gmail.com>
2015-09-01 16:19:26 -0400

    Add ctype for doing things like isdigit, ishex, isalnum, etc

---
+++ libc/libc/ctype.cpp
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2015 Alek Ratzloff
+ *
+ * This file is part of Alek's Little Endian Kernel.
+ * 
+ * Alek's Little Endian Kernel is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Alek's Little Endian Kernel is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Alek's Little Endian Kernel.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "types.h"
+#include "ctype.h"
+
+bool isdigit(s32 c)
+{
+	return (c >= '0' && c <= '9');
+}
