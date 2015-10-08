103908fb6fa517ba545698c0e03110dcefabc1a4
Alek Ratzloff <alekratz@gmail.com>
2015-10-07 22:47:26 -0400

    Rename ByteSize(s) classes to ByteMeasure(s)

--- core/include/sizes.cpp
+++ core/include/sizes.cpp
@@ -1,27 +1,27 @@
 #include "sizes.h"
 
-ByteSuffix<ByteSuffixes::kb> operator "" _kb(u64 value)
-{ return ByteSuffix<ByteSuffixes::kb>(value); }
-ByteSuffix<ByteSuffixes::mb> operator "" _mb(u64 value)
-{ return ByteSuffix<ByteSuffixes::mb>(value); }
-ByteSuffix<ByteSuffixes::gb> operator "" _gb(u64 value)
-{ return ByteSuffix<ByteSuffixes::gb>(value); }
-ByteSuffix<ByteSuffixes::tb> operator "" _tb(u64 value)
-{ return ByteSuffix<ByteSuffixes::tb>(value); }
-ByteSuffix<ByteSuffixes::pb> operator "" _pb(u64 value)
-{ return ByteSuffix<ByteSuffixes::pb>(value); }
-ByteSuffix<ByteSuffixes::yb> operator "" _yb(u64 value)
-{ return ByteSuffix<ByteSuffixes::yb>(value); }
+ByteMeasure<kb> operator "" _kb(u64 value)
+{ return ByteMeasure<kb>(value); }
+ByteMeasure<mb> operator "" _mb(u64 value)
+{ return ByteMeasure<mb>(value); }
+ByteMeasure<gb> operator "" _gb(u64 value)
+{ return ByteMeasure<gb>(value); }
+ByteMeasure<tb> operator "" _tb(u64 value)
+{ return ByteMeasure<tb>(value); }
+ByteMeasure<pb> operator "" _pb(u64 value)
+{ return ByteMeasure<pb>(value); }
+ByteMeasure<yb> operator "" _yb(u64 value)
+{ return ByteMeasure<yb>(value); }
 
-ByteSuffix<ByteSuffixes::kb> operator "" _kb(f128 value)
-{ return ByteSuffix<ByteSuffixes::kb>(value); }
-ByteSuffix<ByteSuffixes::mb> operator "" _mb(f128 value)
-{ return ByteSuffix<ByteSuffixes::mb>(value); }
-ByteSuffix<ByteSuffixes::gb> operator "" _gb(f128 value)
-{ return ByteSuffix<ByteSuffixes::gb>(value); }
-ByteSuffix<ByteSuffixes::tb> operator "" _tb(f128 value)
-{ return ByteSuffix<ByteSuffixes::tb>(value); }
-ByteSuffix<ByteSuffixes::pb> operator "" _pb(f128 value)
-{ return ByteSuffix<ByteSuffixes::pb>(value); }
-ByteSuffix<ByteSuffixes::yb> operator "" _yb(f128 value)
-{ return ByteSuffix<ByteSuffixes::yb>(value); }
+ByteMeasure<kb> operator "" _kb(f128 value)
+{ return ByteMeasure<kb>(value); }
+ByteMeasure<mb> operator "" _mb(f128 value)
+{ return ByteMeasure<mb>(value); }
+ByteMeasure<gb> operator "" _gb(f128 value)
+{ return ByteMeasure<gb>(value); }
+ByteMeasure<tb> operator "" _tb(f128 value)
+{ return ByteMeasure<tb>(value); }
+ByteMeasure<pb> operator "" _pb(f128 value)
+{ return ByteMeasure<pb>(value); }
+ByteMeasure<yb> operator "" _yb(f128 value)
+{ return ByteMeasure<yb>(value); }
--- core/include/sizes.h
+++ core/include/sizes.h
@@ -3,7 +3,7 @@
 
 #include <types.h>
 
-enum class ByteSuffixes : u64
+enum ByteMeasures : u64
 {
   b  =      1,
   kb = b  * 1024,
@@ -14,28 +14,33 @@ enum class ByteSuffixes : u64
   yb = pb * 1024,
 };
 
-template<ByteSuffixes suffix=ByteSuffixes::b>
-struct ByteSuffix
+template<ByteMeasures suffix=b>
+struct ByteMeasure
 {
-  ByteSuffix(f128 value) : value(value) { }
-  ByteSuffix(u64 value) : value(static_cast<f128>(value)) { }
+  ByteMeasure(f128 value) : value(value) { }
+  ByteMeasure(u64 value) : value(static_cast<f128>(value)) { }
   f128 value;
   operator f128() const
-    { return value * static_cast<f128>(suffix); }
+    { return value; }
+  template<ByteMeasures s>
+  ByteMeasure<s> as()
+  {
+    return ByteMeasure<s>(value * (suffix / s));
+  }
 };
 
-ByteSuffix<ByteSuffixes::kb> operator "" _kb(u64 value);
-ByteSuffix<ByteSuffixes::mb> operator "" _mb(u64 value);
-ByteSuffix<ByteSuffixes::gb> operator "" _gb(u64 value);
-ByteSuffix<ByteSuffixes::tb> operator "" _tb(u64 value);
-ByteSuffix<ByteSuffixes::pb> operator "" _pb(u64 value);
-ByteSuffix<ByteSuffixes::yb> operator "" _yb(u64 value);
+ByteMeasure<kb> operator "" _kb(u64 value);
+ByteMeasure<mb> operator "" _mb(u64 value);
+ByteMeasure<gb> operator "" _gb(u64 value);
+ByteMeasure<tb> operator "" _tb(u64 value);
+ByteMeasure<pb> operator "" _pb(u64 value);
+ByteMeasure<yb> operator "" _yb(u64 value);
 
-ByteSuffix<ByteSuffixes::kb> operator "" _kb(f128 value);
-ByteSuffix<ByteSuffixes::mb> operator "" _mb(f128 value);
-ByteSuffix<ByteSuffixes::gb> operator "" _gb(f128 value);
-ByteSuffix<ByteSuffixes::tb> operator "" _tb(f128 value);
-ByteSuffix<ByteSuffixes::pb> operator "" _pb(f128 value);
-ByteSuffix<ByteSuffixes::yb> operator "" _yb(f128 value);
+ByteMeasure<kb> operator "" _kb(f128 value);
+ByteMeasure<mb> operator "" _mb(f128 value);
+ByteMeasure<gb> operator "" _gb(f128 value);
+ByteMeasure<tb> operator "" _tb(f128 value);
+ByteMeasure<pb> operator "" _pb(f128 value);
+ByteMeasure<yb> operator "" _yb(f128 value);
 
 #endif
