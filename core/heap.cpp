40cf8dad93db941d352e0c6190fe86bcfc8c0d63
Alek Ratzloff <alekratz@gmail.com>
2015-11-21 00:54:10 -0500

    Add GPLv3 header to files that were lacking it

--- core/heap.cpp
+++ core/heap.cpp
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2015 Alek Ratzloff
+ *
+ * This file is part of Alek's Little Endian Kernel.
+ * 
+ * Alek's Little Endian Kernel is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Alek's Little Endian Kernel is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Alek's Little Endian Kernel.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "heap.h"
 #include "kernel.h"
 #include "sizes.h"
